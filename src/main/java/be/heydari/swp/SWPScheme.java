aa26774061919c7e84e6e99b3e35585ba971d563
Emad Heydari Beni <emad@heydari.be>
2022-03-07 11:25:58 +0100

    Scheme interfaces

---
+++ src/main/java/be/heydari/swp/SWPScheme.java
@@ -0,0 +1,23 @@
+package be.heydari.swp;
+
+public class SWPScheme {
+
+    public byte[] setup() {
+        // todo
+        return null;
+    }
+
+    public byte[] encrypt(byte[] key, byte[] data) {
+        // todo
+        return null;
+    }
+
+    public byte[] token(byte[] key, byte[] word) {
+        // todo
+        return null;
+    }
+
+    public boolean match(byte[] cipherText, byte[] token) {
+        return false;
+    }
+}
